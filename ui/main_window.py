bf6f5dd0c8375e7adbfbbcfcb7e498a029903bd8
DJP <djpade@gmail.com>
2015-04-20 14:48:11 -0400

    Cleaned up imports and documentation in main_window

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,9 +1,11 @@
+"""
+The plot widget
+"""
+# pylint: disable=no-name-in-module, no-member
 import matplotlib
 from PyQt5.QtCore import QTimer
-from PyQt5.QtWidgets import QApplication, QMainWindow, QMenu, QVBoxLayout, \
-                            QSizePolicy, QMessageBox, QWidget
+from PyQt5.QtWidgets import QSizePolicy
 
-import numpy as np
 from matplotlib.backends.backend_qt5agg \
      import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
@@ -33,13 +35,16 @@ class MainWindow(FigureCanvas):
         timer.timeout.connect(self.update_figure)
         timer.start(50)
 
-        self.x = np.arange(0, 4 * np.pi, 0.1)
-        self.y = np.sin(self.x)
+        # self.x = np.arange(0, 4 * np.pi, 0.1)
+        # self.y = np.sin(self.x)
 
         self.array = array
         self.extent = extent
 
     def update_figure(self):
+        """
+        Update the matplotlib plot
+        """
         # self.axes.plot(self.x, self.y)
         # self.y = np.roll(self.y, -1)
         self.axes.imshow(self.array, extent=self.extent)
