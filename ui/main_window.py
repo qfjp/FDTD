90bc471fe1efe880131069c93de082cfed6e3ed6
DJP <djpade@gmail.com>
2015-04-20 14:48:44 -0400

    Changed init parameters to **kwargs

--- ui/main_window.py
+++ ui/main_window.py
@@ -16,9 +16,34 @@ matplotlib.use('Qt5Agg')
 class MainWindow(FigureCanvas):
     """
     Main window for fdtd simulator
+    :param array: The array to draw using imshow
+    :param extent: The extents to use for drawing the plot
+                   (left, right, bottom, top)
+
+    :param parent: The parent widget
+    :param width: The width of the chart
+    :param height: the height of the chart
+    :param dpi: the resolution of the chart
+    :type array: numpy.ndarray
+    :type extent: list
     """
-    def __init__(self, array=None, extent=None, parent=None, width=5,
-                 height=4, dpi=100):
+    def __init__(self, **kwargs):
+        # Default kwargs to use:
+        array = None
+        extent = None
+        parent = None
+        width = 5
+        height = 4
+        dpi = 100
+        try:
+            array = kwargs['array']
+            extent = kwargs['extent']
+            parent = kwargs['parent']
+            width = kwargs['width']
+            height = kwargs['height']
+            dpi = kwargs['dpi']
+        except KeyError:
+            pass
         fig = Figure(figsize=(width, height), dpi=dpi)
         self.axes = fig.add_subplot(111)
         # Clear the axes every time plot() is called
